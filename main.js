a8f4c215894833d8a40161b168019266cfd78777
Anvar Lapasov <lapasovanvar2908@gmail.com>
2021-12-02 20:20:33 +0300

    final

--- main.js
+++ main.js
@@ -1,6 +1,4 @@
-// let API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e'
 
-// let API_KEY = 'b971c2f0de8767f08d2bb84160ba24b7'
 let list = document.querySelector('.append')
 let btntop = document.querySelector('#top')
 let btnpopular = document.querySelector('#popular')
