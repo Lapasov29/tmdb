ded5983db150a619756b91ede2a4de32549fb23b
Anvar Lapasov <lapasovanvar2908@gmail.com>
2021-12-02 18:27:06 +0300

    api are ready

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-let API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e'
+// let API_KEY = 'dcea1fd7b3e65d34387ad6de7ef9cc5e'
 
-let API_KEY = 'b971c2f0de8767f08d2bb84160ba24b7'
+// let API_KEY = 'b971c2f0de8767f08d2bb84160ba24b7'
+let list = document.querySelector('.append')
+
+async function render(params) {
+    let api = await fetch('https://api.themoviedb.org/3/movie/popular?api_key=dcea1fd7b3e65d34387ad6de7ef9cc5e&page=1')
+    let res = await api.json()
+    console.log(res);
+}
+
+render()
